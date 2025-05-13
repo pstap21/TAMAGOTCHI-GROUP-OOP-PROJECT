1804c0746e00df3ee86637447b98f4d15b2a2579
pstap21 <a1629853@adelaide.edu.au>
2025-05-13 12:01:40 +0930

    basepet update

---
+++ BasePet.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class BasePet {
+    public:
+
+    int hunger();
+    int energy();
+    int cleanliness();
+    int happiness();
+    bool is_alive();
+    int age();
+    std::string name();
+
+
+};
